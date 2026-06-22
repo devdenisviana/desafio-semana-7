a3ef2c8b491d425ebc3578263b81d164920593ba
Denis Viana <41395264+devdenisviana@users.noreply.github.com>
2026-06-22 19:28:06 -0300

    Desafio Semana 7 - 10 desafios em C#

---
+++ Desafio01/Program.cs
@@ -0,0 +1,30 @@
+﻿static double CalcularMedia(double nota1, double nota2)
+{
+    return (nota1 + nota2) / 2;
+}
+
+static string VerificarSituacao(double media)
+{
+    if (media >= 7)
+        return "Aprovado";
+    else if (media >= 5)
+        return "Recuperação";
+    else
+        return "Reprovado";
+}
+
+Console.Write("Nome do aluno: ");
+string nome = Console.ReadLine();
+
+Console.Write("Nota 1: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.Write("Nota 2: ");
+double nota2 = double.Parse(Console.ReadLine());
+
+double media = CalcularMedia(nota1, nota2);
+string situacao = VerificarSituacao(media);
+
+Console.WriteLine($"\nAluno: {nome}");
+Console.WriteLine($"Média: {media:F1}");
+Console.WriteLine($"Situação: {situacao}");
---
+++ Desafio02/Program.cs
@@ -0,0 +1,21 @@
+﻿static string VerificarEstoque(int quantidade)
+{
+    if (quantidade < 5)
+        return "Estoque Crítico";
+    else if (quantidade <= 10)
+        return "Estoque Baixo";
+    else
+        return "Estoque OK";
+}
+
+Console.Write("Nome do produto: ");
+string produto = Console.ReadLine();
+
+Console.Write("Quantidade atual: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+string status = VerificarEstoque(quantidade);
+
+Console.WriteLine($"\nProduto: {produto}");
+Console.WriteLine($"Quantidade: {quantidade}");
+Console.WriteLine($"Status: {status}");
